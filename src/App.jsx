d8fa1e1bfa9921ed707607aa63f1ba646becae9c
Nacershucks <zack.sawyerr@gmail.com>
2024-03-11 21:53:04 +0000

    arranges children of App.jsx and UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import './api.js'
 import './App.css'
 
+import { UserContext } from './contexts/UserContext.jsx'
+
+import ArticleList from './components/ArticleList.jsx'
+import ArticleDetail from './components/ArticleDetail.jsx'
+import Login from './components/Login.jsx'
+import Navbar from './components/Navbar.jsx'
+import UserDetail from './components/UserDetail.jsx'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(false)
 
   return (
-    <>
-      <h1>NC NEWS</h1>
-    </>
+    <UserContext.Provider value={{user}}>
+      <Navbar/>
+      <Routes>
+        <Route path='/articles' element={<ArticleList/>}/>
+        <Route path='/articles/:articleId' element={<ArticleDetail/>}/>
+        <Route path='/login' element={<Login setUser={setUser}/>}/>
+        <Route path='/user/:userId' element={<UserDetail/>}/>
+      </Routes>
+    </UserContext.Provider>
+    
   )
 }
 
