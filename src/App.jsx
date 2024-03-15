fc99a0c930a6685a6cc1ad61a74ba390697a2136
Nacershucks <zack.sawyerr@gmail.com>
2024-03-15 11:44:36 +0000

    testing implementation of Login failing at api request

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,7 @@ import UserDetail from './components/UserDetail.jsx'
 import ErrorPage from './components/ErrorPage.jsx'
 
 function App() {
-  const [user, setUser] = useState({
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
-  })
+  const [user, setUser] = useState(false)
 
   return (
     <UserContext.Provider value={{user, setUser}}>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react"
+import { getUser} from "../api"
 
-export default function Login (){
+export default function Login (props){
+    const { setUser } = props
+    const [nameInput, setNameInput] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [loadingMsg, setLoadingMsg] = useState("")
+
+    const handleChange = (e) => {
+        setIsLoading(false)
+        setNameInput(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setNameInput("")
+        setLoadingMsg("logging in...")
+        setIsLoading(true)
+        getUser(nameInput).then((user) => {
+            setUser(user)
+        }).catch((err) => {
+            setLoadingMsg("invalid username")
+        })
+    }
+
+    const loginLoader = () => {
+        if(!isLoading){
+           return <button type={"submit"}>submit</button>
+        }else if(isLoading){
+           return <p>{loadingMsg}</p>
+        }
+    }
+
     return (
-        <>
-        </>
+        <form onSubmit={handleSubmit}>
+            <label for="fUsername">username:</label>
+            <input type="text" id="fUsername" name="fUsername" onChange={handleChange} value={nameInput}></input>
+            {loginLoader()}
+        </form>
     )
 }
